f3316c3d4b9bde805762d5c7e25e9ba39c751717
ansonkan <48442757+ansonkan@users.noreply.github.com>
2024-11-23 22:25:27 +0800

    first commit

---
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@onready var visuals: Node2D = $Visuals
+@onready var velocity_component: VelocityComponent = $VelocityComponent
+
+
+func _process(delta: float) -> void:
+	velocity_component.accelerate_to_player()
+	velocity_component.move(self)
+	
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(-move_sign, 1)
+	
