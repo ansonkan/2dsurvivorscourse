23e059bceb5db3722869daf0dbcc06530ef9e239
ansonkan <48442757+ansonkan@users.noreply.github.com>
2024-12-20 12:23:56 +0800

    up to lesson 61

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 125
-const ACCELERATION_SMOOTHING = 25
-
 @onready var damage_interval_timer = $DamageIntervalTimer
 @onready var health_component = $HealthComponent
 @onready var health_bar = $HealthBar
 @onready var abilities = $Abilities
 @onready var animation_player = $AnimationPlayer
 @onready var visuals = $Visuals
+@onready var velocity_component: VelocityComponent = $VelocityComponent
+
 
 var number_colliding_bodies = 0
+var base_speed = 0
+
 
 func _ready() -> void:
+	base_speed = velocity_component.max_speed
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
@@ -25,9 +27,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
-	var target_velocity = direction * MAX_SPEED
-	velocity	 = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
-	move_and_slide()
+	velocity_component.accelerate_in_direction(direction)
+	velocity_component.move(self)
 	
 	if movement_vector.x != 0 || movement_vector.y != 0:
 		animation_player.play("walk")
@@ -70,12 +71,16 @@ func on_damage_interval_timer_timeout():
 
 
 func on_health_changed():
+	GameEvents.emit_player_damaged()
 	update_health_display()
+	$HitRandomStreamPlayer.play_random()
 
 
 func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrade: Dictionary):
-	if upgrade == null || not upgrade is Ability:
-		return
+	if upgrade is Ability:
+		abilities.add_child((upgrade as Ability).ability_controller_scene.instantiate())
+	elif upgrade.id == "player_speed":
+		velocity_component.max_speed = base_speed + (base_speed * current_upgrade["player_speed"]["quantity"] * .1)
 		
-	abilities.add_child((upgrade as Ability).ability_controller_scene.instantiate())
+	
 	
--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -1,8 +1,16 @@
 extends CanvasLayer
 
+@onready var panel_container = $"%PanelContainer"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	panel_container.pivot_offset = panel_container.size / 2
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	get_tree().paused = true
 	$"%RestartButton".pressed.connect(on_restart_button_pressed)
 	$"%QuitButton".pressed.connect(on_quit_button_pressed)
@@ -11,6 +19,14 @@ func _ready() -> void:
 func set_defeat():
 	$"%TitleLabel".text = "Defeat"
 	$"%DescriptionLabel".text = "You lost!"
+	play_jingle(true)
+
+
+func play_jingle(defeat: bool = false):
+	if defeat:
+		$DefectStreamPlayer.play()
+	else:
+		$VictoryStreamPlayer.play()
 
 
 func on_restart_button_pressed():
